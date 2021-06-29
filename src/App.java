991962ed773fe805ad89cdc7301a46889e88acf5
amcdvg <alexander.moreno@utp.edu.co>
2021-06-29 10:07:16 -0500

    clase2 unit 1 ciclo2

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int [] lista = {2,3,4,5,6};
+        System.out.println(lista[0]);
+    }
+}
---
+++ src/Junio29.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Junio29 {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Dame tu nombre");
+        String nombre = scanner.nextLine();
+        System.out.println("Hola " + nombre);
+
+    }
+public static String saludar(String nombre){
+    return "Hola " + nombre;
